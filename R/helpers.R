424070b8b2f1ee250620f98517c90e5f85b0b4c8
Richard Iannone <riannone@me.com>
2018-08-05 23:48:32 -0700

    Add helper fcns to specify fixed-time formats

--- R/helpers.R
+++ R/helpers.R
@@ -7,3 +7,23 @@ fdf <- function(format_name) {
   class(x) <- "fdf"
   x
 }
+
+#' @export
+ftf_12 <- function(format_name) {
+
+  # Create a character object
+  x <- format_name
+
+  class(x) <- "ftf_12"
+  x
+}
+
+#' @export
+ftf_24 <- function(format_name) {
+
+  # Create a character object
+  x <- format_name
+
+  class(x) <- "ftf_24"
+  x
+}
