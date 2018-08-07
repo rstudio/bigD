89675590924025c63e48d0359b3f372c7d69ea1c
Richard Iannone <riannone@me.com>
2018-08-06 23:23:11 -0700

    Allow for numeric inputs to `format`

--- R/helpers.R
+++ R/helpers.R
@@ -1,28 +1,64 @@
 #' @export
-fdf <- function(format_name) {
+fdf <- function(format) {
+
+  if (inherits(format, "numeric") &&
+      !(format_name %in% seq(fdf_types()))) {
+
+    stop("The supplied format number is not within the range of 1-",
+         length(fdf_types()), ".")
+
+  } else if (inherits(format, "numeric") &&
+             format %in% seq(fdf_types())) {
+
+    format <- fdf_types()[format]
+  }
 
   # Create a character object
-  x <- format_name
+  x <- format
 
   class(x) <- "fdf"
   x
 }
 
 #' @export
-ftf_12 <- function(format_name) {
+ftf_12 <- function(format) {
+
+  if (inherits(format, "numeric") &&
+      !(format_name %in% seq(ftf_12_types()))) {
+
+    stop("The supplied format number is not within the range of 1-",
+         length(ftf_12_types()), ".")
+
+  } else if (inherits(format, "numeric") &&
+             format %in% seq(ftf_12_types())) {
+
+    format <- ftf_12_types()[format]
+  }
 
   # Create a character object
-  x <- format_name
+  x <- format
 
   class(x) <- "ftf_12"
   x
 }
 
 #' @export
-ftf_24 <- function(format_name) {
+ftf_24 <- function(format) {
+
+  if (inherits(format, "numeric") &&
+      !(format_name %in% seq(ftf_24_types()))) {
+
+    stop("The supplied format number is not within the range of 1-",
+         length(ftf_24_types()), ".")
+
+  } else if (inherits(format, "numeric") &&
+             format %in% seq(ftf_24_types())) {
+
+    format <- ftf_24_types()[format]
+  }
 
   # Create a character object
-  x <- format_name
+  x <- format
 
   class(x) <- "ftf_24"
   x
