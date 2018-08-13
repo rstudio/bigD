5444cc3e95927db49b736dcf61a5fab5f819e90b
Richard Iannone <riannone@me.com>
2018-08-12 19:49:03 -0700

    Add info fcn `info_date_time_combine()`

--- R/info_fcns.R
+++ R/info_fcns.R
@@ -1,6 +1,6 @@
 #' Get information on the different flexible date formats
 #' @param locale an optional output locale for inspecting the \code{input} value
-#' across all of the different fdf types. Example locale names include
+#' across all of the different formatting types. Example locale names include
 #' \code{"en_US"} for English (United States) and \code{"fr_FR"} for French
 #' (France). If a locale name is not provided then \code{"en_US"} will be used.
 #' @param input an optional input date-time string. The appropriate
@@ -30,7 +30,7 @@ info_fdf_types <- function(locale = NULL,
 
     outputs <-
       c(outputs,
-        format_w_pattern(
+        fmt_date_time(
           input = input,
           date_format = fdf(fdf_type),
           time_format = NULL,
@@ -40,22 +40,12 @@ info_fdf_types <- function(locale = NULL,
 
   message(
     paste0("For the locale '", locale, "' and the input of '", input, "',\n",
-           "the following output dates can be made by each fdf type:\n"),
+           "the following output dates can be made by each `fdf` type:\n"),
     paste0("(", seq_along(fdf_types), ") ", fdf_types, ": ", outputs, "\n"))
 }
 
 #' Get information on the different 12-hour flexible time formats
-#' @param locale an optional output locale for inspecting the \code{input} value
-#' across all of the different fdf types. Example locale names include
-#' \code{"en_US"} for English (United States) and \code{"fr_FR"} for French
-#' (France). If a locale name is not provided then \code{"en_US"} will be used.
-#' @param input an optional input date-time string. The appropriate
-#' representation should use the following construction outlined in the ISO
-#' 8601:2004 standard: \code{YYYY-MM-DDThh:mm:ss.sTZD} although some
-#' allowances made here to ease this restrictiveness (for example, the literal
-#' \code{T} separating the date and time components is optional). Fractional
-#' seconds are optional as is the time-zone designation (TZD). If a value is
-#' not provided then a default date-time string will be used.
+#' @inheritParams info_fdf_types
 #' @export
 info_ftf_12_types <- function(locale = NULL,
                               input = NULL) {
@@ -76,7 +66,7 @@ info_ftf_12_types <- function(locale = NULL,
 
     outputs <-
       c(outputs,
-        format_w_pattern(
+        fmt_date_time(
           input = input,
           date_format = NULL,
           time_format = ftf_12(ftf_12_type),
@@ -86,22 +76,12 @@ info_ftf_12_types <- function(locale = NULL,
 
   message(
     paste0("For the locale '", locale, "' and the input of '", input, "',\n",
-           "the following output times can be made by each ftf_12 type:\n"),
+           "the following output times can be made by each `ftf_12` type:\n"),
     paste0("(", seq_along(ftf_12_types), ") ", ftf_12_types, ": ", outputs, "\n"))
 }
 
 #' Get information on the different 24-hour flexible time formats
-#' @param locale an optional output locale for inspecting the \code{input} value
-#' across all of the different fdf types. Example locale names include
-#' \code{"en_US"} for English (United States) and \code{"fr_FR"} for French
-#' (France). If a locale name is not provided then \code{"en_US"} will be used.
-#' @param input an optional input date-time string. The appropriate
-#' representation should use the following construction outlined in the ISO
-#' 8601:2004 standard: \code{YYYY-MM-DDThh:mm:ss.sTZD} although some
-#' allowances made here to ease this restrictiveness (for example, the literal
-#' \code{T} separating the date and time components is optional). Fractional
-#' seconds are optional as is the time-zone designation (TZD). If a value is
-#' not provided then a default date-time string will be used.
+#' @inheritParams info_fdf_types
 #' @export
 info_ftf_24_types <- function(locale = NULL,
                               input = NULL) {
@@ -122,7 +102,7 @@ info_ftf_24_types <- function(locale = NULL,
 
     outputs <-
       c(outputs,
-        format_w_pattern(
+        fmt_date_time(
           input = input,
           date_format = NULL,
           time_format = ftf_24(ftf_24_type),
@@ -132,6 +112,44 @@ info_ftf_24_types <- function(locale = NULL,
 
   message(
     paste0("For the locale '", locale, "' and the input of '", input, "',\n",
-           "the following output times can be made by each ftf_24 type:\n"),
+           "the following output times can be made by each `ftf_24` type:\n"),
     paste0("(", seq_along(ftf_24_types), ") ", ftf_24_types, ": ", outputs, "\n"))
 }
+
+#' Get information on the date-time combining formats
+#' @inheritParams info_fdf_types
+#' @export
+info_date_time_combine <- function(locale = NULL,
+                                   input = NULL) {
+
+  if (is.null(locale)) {
+    locale <- "en"
+  }
+
+  if (is.null(input)) {
+    input <- "2018-07-24T14:44:22.234343-0800(America/Vancouver)"
+  }
+
+  date_time_combine_types <- date_time_combine_types()
+
+  outputs <- c()
+
+  for (date_time_combine_type in date_time_combine_types) {
+
+    outputs <-
+      c(outputs,
+        fmt_date_time(
+          input = input,
+          date_format = fdf(format = 1),
+          time_format = ftf_24(format = 1),
+          combination = date_time_combine(date_time_combine_type),
+          locale = locale))
+  }
+
+  message(
+    paste0("For the locale '", locale, "' and the input of '", input, "',\n",
+           "the following date-times can be made by each `date_time_combine` type:\n"),
+    paste0("(", seq_along(date_time_combine_types), ") ", date_time_combine_types, ": ",
+           outputs, "\n"))
+}
+
