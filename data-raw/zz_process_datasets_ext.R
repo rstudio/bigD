bb0243da534f86b8a2680e9a7a1e66a184bbe3f3
Richard Iannone <riannone@me.com>
2022-08-07 17:21:12 -0400

    Update zz_process_datasets_ext.R

--- data-raw/zz_process_datasets_ext.R
+++ data-raw/zz_process_datasets_ext.R
@@ -8,11 +8,12 @@ source("data-raw/X05-tz_metazone_users.R")
 source("data-raw/X06-tz_map.R")
 source("data-raw/X07-tz_formats.R")
 source("data-raw/X08-tz_bcp_id.R")
+source("data-raw/X09-tz_name_resolution.R")
 
 # Create external datasets
 usethis::use_data(
   tzdb, dates, tz_exemplar,
   tz_metazone_names, tz_metazone_users,
-  tz_map, tz_formats, tz_bcp_id,
+  tz_map, tz_formats, tz_bcp_id, tz_name_resolution,
   internal = TRUE, overwrite = TRUE
 )
