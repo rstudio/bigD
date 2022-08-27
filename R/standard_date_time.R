b5bffda33b667960927e02fef45f787ba550b8d6
Richard Iannone <riannone@me.com>
2022-08-27 17:24:08 -0400

    Update standard_date_time.R

--- R/standard_date_time.R
+++ R/standard_date_time.R
@@ -1,8 +1,12 @@
 #' Obtain a standard datetime format that works across locales
 #'
+#' @param type One of four standardized types for the resulting datetime that
+#'   range in precision and verbosity. These are `"short"` (the default),
+#'   `"medium"`, `"long"`, and `"full"`.
+#'
 #' @export
 standard_date_time <- function(
-    type = c("full", "long", "medium", "short")
+    type = c("short", "medium", "long", "full")
 ) {
   type <- match.arg(type)
   x <- "sdt"
@@ -11,6 +15,10 @@ standard_date_time <- function(
 }
 
 #' Obtain a standard date format that works across locales
+#'
+#' @param type One of four standardized types for the resulting date that range
+#'   in precision and verbosity. These are `"short"` (the default), `"medium"`,
+#'   `"long"`, and `"full"`.
 #'
 #' @export
 standard_date <- function(
@@ -23,6 +31,10 @@ standard_date <- function(
 }
 
 #' Obtain a standard time format that works across locales
+#'
+#' @param type One of four standardized types for the resulting time that range
+#'   in precision and verbosity. These are `"short"` (the default), `"medium"`,
+#'   `"long"`, and `"full"`.
 #'
 #' @export
 standard_time <- function(
