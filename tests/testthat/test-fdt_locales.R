b75cc65bc399ce6d123929cbe9f877bc1c0f045d
Richard Iannone <riannone@me.com>
2022-08-29 19:52:31 -0400

    Create test-fdt_locales.R

---
+++ tests/testthat/test-fdt_locales.R
@@ -0,0 +1,29 @@
+test_that("A vector can be obtained from the `fdt_locales_vec()` function", {
+
+  expect_vector(fdt_locales_vec())
+  expect_type(fdt_locales_vec(), "character")
+  expect_length(fdt_locales_vec(), 574)
+})
+
+test_that("The `fdt_locales_lst` object has a particular structure", {
+
+  expect_type(fdt_locales_lst, "list")
+  expect_equal(names(fdt_locales_lst), fdt_locales_vec())
+  expect_equal(unname(unlist(fdt_locales_lst)), fdt_locales_vec())
+})
+
+test_that("The `fdt_locales_lst` object can be used with `fdt()`", {
+
+  for (i in seq_along(fdt_locales_vec())) {
+
+      fmt_val <-
+        fdt(
+          input = "2018-07-04 22:05",
+          locale = fdt_locales_lst[[i]]
+        )
+
+      expect_vector(fmt_val)
+      expect_length(fmt_val, 1)
+      expect_type(fmt_val, "character")
+  }
+})
