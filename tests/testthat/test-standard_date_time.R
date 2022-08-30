66683ebf6ddbec9d334c46f2917c290b23420e74
Richard Iannone <riannone@me.com>
2022-08-29 22:27:32 -0400

    Update test-standard_date_time.R

--- tests/testthat/test-standard_date_time.R
+++ tests/testthat/test-standard_date_time.R
@@ -36,65 +36,65 @@ test_that("The `standard_*()` functions produce very specific objects", {
 
 test_that("The `standard_*()` functions can be used in `fdt()`", {
 
-  # expect_equal(
-  #   vapply(
-  #     c("short", "medium", "long", "full"),
-  #     FUN.VALUE = character(1),
-  #     USE.NAMES = FALSE,
-  #     FUN = function(x) {
-  #       fdt(
-  #         input = "2018-07-04 22:05 (America/Los_Angeles)",
-  #         format = standard_date_time(type = x)
-  #       )
-  #     }
-  #   ),
-  #   c(
-  #     "7/4/18, 10:05 PM",
-  #     "Jul 4, 2018, 10:05:00 PM",
-  #     "July 4, 2018 at 10:05:00 PM PDT",
-  #     "Wednesday, July 4, 2018 at 10:05:00 PM Pacific Daylight Time"
-  #   )
-  # )
+  expect_equal(
+    vapply(
+      c("short", "medium", "long", "full"),
+      FUN.VALUE = character(1),
+      USE.NAMES = FALSE,
+      FUN = function(x) {
+        fdt(
+          input = "2018-07-04 22:05 (America/Los_Angeles)",
+          format = standard_date_time(type = x)
+        )
+      }
+    ),
+    c(
+      "7/4/18, 10:05 PM",
+      "Jul 4, 2018, 10:05:00 PM",
+      "July 4, 2018 at 10:05:00 PM PDT",
+      "Wednesday, July 4, 2018 at 10:05:00 PM Pacific Daylight Time"
+    )
+  )
 
-  # expect_equal(
-  #   vapply(
-  #     c("short", "medium", "long", "full"),
-  #     FUN.VALUE = character(1),
-  #     USE.NAMES = FALSE,
-  #     FUN = function(x) {
-  #       fdt(
-  #         input = "2018-07-04 22:05 (America/Los_Angeles)",
-  #         format = standard_date(type = x)
-  #       )
-  #     }
-  #   ),
-  #   c(
-  #     "7/4/18",
-  #     "Jul 4, 2018",
-  #     "July 4, 2018",
-  #     "Wednesday, July 4, 2018"
-  #   )
-  # )
+  expect_equal(
+    vapply(
+      c("short", "medium", "long", "full"),
+      FUN.VALUE = character(1),
+      USE.NAMES = FALSE,
+      FUN = function(x) {
+        fdt(
+          input = "2018-07-04 22:05 (America/Los_Angeles)",
+          format = standard_date(type = x)
+        )
+      }
+    ),
+    c(
+      "7/4/18",
+      "Jul 4, 2018",
+      "July 4, 2018",
+      "Wednesday, July 4, 2018"
+    )
+  )
 
-  # expect_equal(
-  #   vapply(
-  #     c("short", "medium", "long", "full"),
-  #     FUN.VALUE = character(1),
-  #     USE.NAMES = FALSE,
-  #     FUN = function(x) {
-  #       fdt(
-  #         input = "2018-07-04 22:05 (America/Los_Angeles)",
-  #         format = standard_time(type = x)
-  #       )
-  #     }
-  #   ),
-  #   c(
-  #     "10:05 PM",
-  #     "10:05:00 PM",
-  #     "10:05:00 PM PDT",
-  #     "10:05:00 PM Pacific Daylight Time"
-  #   )
-  # )
+  expect_equal(
+    vapply(
+      c("short", "medium", "long", "full"),
+      FUN.VALUE = character(1),
+      USE.NAMES = FALSE,
+      FUN = function(x) {
+        fdt(
+          input = "2018-07-04 22:05 (America/Los_Angeles)",
+          format = standard_time(type = x)
+        )
+      }
+    ),
+    c(
+      "10:05 PM",
+      "10:05:00 PM",
+      "10:05:00 PM PDT",
+      "10:05:00 PM Pacific Daylight Time"
+    )
+  )
 
   select_locales <-
     c(
