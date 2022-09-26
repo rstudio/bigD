0c2684cb2b98e1f2360b6dfecc442e6911b0b51f
Richard Iannone <riannone@me.com>
2022-09-25 21:28:30 -0400

    Update zz_process_datasets_ext.R

--- data-raw/zz_process_datasets_ext.R
+++ data-raw/zz_process_datasets_ext.R
@@ -11,11 +11,12 @@ source("data-raw/X08-tz_map.R")
 source("data-raw/X09-tz_formats.R")
 source("data-raw/X10-tz_bcp_id.R")
 source("data-raw/X11-tz_name_resolution.R")
+source("data-raw/X12-default_locales.R")
 
 # Create external datasets
 usethis::use_data(
   tzdb, dates, day_periods, start_of_week,
   tz_exemplar, tz_metazone_names, tz_metazone_users,
-  tz_map, tz_formats, tz_bcp_id, tz_name_resolution,
+  tz_map, tz_formats, tz_bcp_id, tz_name_resolution, default_locales,
   internal = TRUE, overwrite = TRUE
 )
